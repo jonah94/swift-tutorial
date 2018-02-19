afe869de8ff96338146c6777bddf1ca1a27f6a1b
Jonah Willis <jonahwillis@Jonahs-MBP.fios-router.home>
2018-02-18 21:39:23 -0500

    Building the Basic UI

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jonah Willis on 2/18/18.
+//  Copyright © 2018 Jonah Willis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
